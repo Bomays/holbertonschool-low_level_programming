ec1e8d0da343cf0a57a2b4d5c511b9e4e72918c1
Bomays <9314@holbertonstudents.com>
2024-06-17 16:31:28 +0200

    printf command in C instruction - correction

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -7,8 +7,7 @@
 
 int main(void)
 {
-	printf("Write a C program that prints exactly with proper grammar,
-		but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
+
 	return (0);
 }
-
