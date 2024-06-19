d9e16f40194b205d3a50520736e3dcbb8d3b0355
Bomays <9314@holbertonstudents.com>
2024-06-19 10:28:26 +0200

    3-print_alphabets.c - program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,20 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	char (letter);
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
