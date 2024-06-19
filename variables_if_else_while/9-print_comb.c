a22a5e7579dd1f3f05bb93d5fd8a1dc24611d301
Bomays <9314@holbertonstudents.com>
2024-06-19 16:24:27 +0200

    9-print_comb - program that prints all possible combinations of single-digit numbers separated by a coma and a space

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,1 +1,28 @@
-#!/bin/bash
+/*
+ * program that prints all possible combinations of single-digit numbers :
+ * 0, 1, 2, 3, 4, 5, 6, 7, 8, 9$
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+	int main(void)
+{
+		int num;
+
+			for (num = '0'; num <= '9'; num++)
+		{
+			putchar((num % 10) + '0');
+			putchar(',');
+			putchar(' ');
+		}
+			putchar('\n');
+
+	return (0);
+}
