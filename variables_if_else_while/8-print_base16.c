5fd7615a534b8a7de089acd87aac836afea6f86b
Bomays <9314@holbertonstudents.com>
2024-06-19 15:17:23 +0200

    8-print_base16.c - program that prints all the numbers of base 16 in lowercase using only three times putchar.

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,1 +1,23 @@
-#!/bin/bash
+/* program that prints all the numbers of base 16 in lowercase */
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+
+	int main(void)
+{
+
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	for (letter = 'a'; letter < 'g'; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
