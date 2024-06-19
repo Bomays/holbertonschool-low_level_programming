a547dcf5eebaaf64fb72da8232d842a1f7313406
Bomays <9314@holbertonstudents.com>
2024-06-19 12:23:10 +0200

    6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,20 @@
-#!/bin/bash
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ *
+ */
+
+	int main(void)
+{
+		int i;
+
+		for (i >= 0; i++)
+			putchar((i % 10) + '0');
+			putchar('\n');
+	
+	return(0);
+}
