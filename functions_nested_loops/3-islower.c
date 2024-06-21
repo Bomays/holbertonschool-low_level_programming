6e33cf937bd56e3b0d15788a801ad461249c87f6
Bomays <9314@holbertonstudents.com>
2024-06-21 15:17:24 +0200

    3-islower - prototype function that checks for lowercase character with a condition

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,12 +1,18 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
+#include "main.h"
+                                                                                            
 /**
- * main - Entry point
- *
- * Return: Always 0
- *
+ * int _islower(int c) - Check description
+ * Description: it checks for lowercase character
+ * Return: 0
  */
-
-int main(void)
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		{
+		return 1;
+		}
+	else
+	{
+		return 0;
+	}
+}
