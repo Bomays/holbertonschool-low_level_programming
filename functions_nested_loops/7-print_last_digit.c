9f35cdedb3d8935951d688a8560f37cd8d710045
Bomays <9314@holbertonstudents.com>
2024-06-23 14:00:02 +0200

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,33 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - Entry point
+ * print_last_digit - print the last digit of a number.
  *
- * Return: Always 0
+ * @r: integer value
  *
+ * Explication :
+ * if (r < 0) n = -1 * (r % 10);
+ * if 'r' is more than 0,(r % 10) takes the last number of the argument and
+ * multiply it with -1 to have a positive integer
+ *
+ * _putchar((n % 10) + '0');
+ * it converts the last number of 'n' thanks to modulo operator '%'
+ * (which gives the remainder of the division of 'n' by 10),
+ * in a caracter with "+0", and print it with '_putchar'.
+ *
+ * Return: return the value of last digit
  */
 
-int main(void)
+int print_last_digit(int r)
+{
+	int n;
+
+	if (r < 0)
+		n = -1 * (r % 10);
+
+	else
+		n = r % 10;
+
+	_putchar((n % 10) + '0');
+
+	return (n % 10);
+}
