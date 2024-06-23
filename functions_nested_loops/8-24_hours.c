49e09c2f9026e1598b8a09a0fe2777466aaf1c9d
Bomays <9314@holbertonstudents.com>
2024-06-23 14:53:58 +0200

    8-24_hours.c : prints every minute of the day of Jack Bauer,starting from 00:00 to 23:59 - 'For-For' nested loop printing only with putchar

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,12 +1,31 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - Entry point
+ * jack_bauer - prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59.
  *
- * Return: Always 0
+ * Explication:
+ * ((Hr / 10  + '0'); give the first number thanks
+ * to the division and convert it in caracter thanks to '+0'
+ * ((Hr % 10) + '0'); give the second number tanks to
+ * the reminder of the modulo calcul and convert it in caracter
  *
+ * Return: no return
  */
 
-int main(void)
+void jack_bauer(void)
+{
+	int Hr;
+	int Min;
+
+	for (Hr = 0; Hr < 24; Hr++)
+
+		for (Min = 0; Min < 60; Min++)
+	{
+			_putchar((Hr / 10)  + '0');
+			_putchar((Hr % 10) + '0');
+			_putchar(':');
+			_putchar((Min / 10) + '0');
+			_putchar((Min % 10) + '0');
+			_putchar('\n');
+	}
+}
