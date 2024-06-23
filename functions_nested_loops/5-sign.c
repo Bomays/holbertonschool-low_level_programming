a13fa126922f94dc13b9e5e1a4406c9ef3a63b7f
Bomays <9314@holbertonstudents.com>
2024-06-23 09:27:53 +0200

    5-sign.c : function that prints the sign of a number using only putchar and not printf.

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,12 +1,29 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - Entry point
+ * print_sign - prints the sign of a number.
+ * @n : prints number sign
  *
- * Return: Always 0
- *
+ * Return:
+ * Returns 1 and prints + if n is greater than zero
+ * Returns 0 and prints 0 if n is zero
+ * Returns -1 and prints - if n is less than zero
  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
 
-int main(void)
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
