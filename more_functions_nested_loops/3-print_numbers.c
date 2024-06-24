da4918320a3d8c40e6d32f6708d6ca18ed017875
Bomays <9314@holbertonstudents.com>
2024-06-24 15:09:29 +0200

    3-print_numbers.c : function that prints the numbers, from 0 to 9, followed by a new line, using only two times putchar

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - checks for uppercase charater
+ *
+ * Description : function that prints the numbers,
+ * from 0 to 9, followed by a new line.
+ *
+ * Return: return 0
+ */
+void print_numbers(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		_putchar(number);
+		_putchar('\n');
+	}
+}
