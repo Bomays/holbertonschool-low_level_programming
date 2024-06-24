f1bedcfdc961ed836ee6ede8c482169d615d1b86
Bomays <9314@holbertonstudents.com>
2024-06-24 14:27:04 +0200

    1-isdigit.c : function that checks for a digit (0 through 9), 2-mul.c :function that multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers.
+ * @a: number a
+ * @b: number b
+ * Return: return the result of axb calculation
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
