c7d84010781b44c65f5727b4e989e2825e42c8c8
Bomays <9314@holbertonstudents.com>
2024-06-24 10:46:36 +0200

    11-print_to_98.c : correcting definition

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (n = n; n <= 97; n++)
+		for (n = n; n < 98; n++)
 		{
 			printf("%d, ", n);
 
@@ -25,7 +25,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (n = n; n >= 97; n--)
+		for (n = n; n > 98; n--)
 		{
 			printf("%d, ", n);
 
