b6223586629998b2c9e9574ccf9797572de4f3de
Bomays <9314@holbertonstudents.com>
2024-06-25 14:14:06 +0200

    9 Fizz_Buzz TEST: filter out the 99.5% of programming job candidates who can’t seem to program their way out of a wet paper bag.

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_fizzbuzz - print the numbers for filtering candidates
+ *
+ * Return: Always 0
+ **/
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0)
+			printf(" Fizz ");
+
+		else if (num % 5 == 0)
+			printf(" Buzz ");
+
+		else if (num % 3 == 0 && num % 5 == 0)
+			printf(" FizzBuzz ");
+		else
+			printf("%d ", num);
+	}
+	printf("\n");
+
+	return (0);
+}
