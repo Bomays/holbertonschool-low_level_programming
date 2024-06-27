415ef3e368b73a23b36655aa29546fd725b24692
Bomays <9314@holbertonstudents.com>
2024-06-27 12:57:21 +0200

    3-puts.c: function that prints a string followed by a new line, to stdout

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,12 +1,18 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - Entry point
+ * _puts - it prints a string followed by a new line to stdout
+ * @str: The string to print
  *
  * Return: Always 0
- *
  */
+void _puts(char *str)
+{
+	while (*str)/*loops trough the string*/
+	{
+		_putchar(*str);
+		str++;
+		/*prints the string browsing each carater with str++*/
+		}
+	_putchar('\n');/*new line to stdout*/
+}
 
-int main(void)
