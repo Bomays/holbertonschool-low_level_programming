b3148bd130960710983ef3324a9bbc15f514c630
Bomays <9314@holbertonstudents.com>
2024-06-28 11:22:40 +0200

    6-puts2.c : long string correction output2 adding even carater finding

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,12 +10,13 @@ void puts2(char *str)
 {
 	int a;
 
-	for (a = 0; str[a] != '\0'; a += 2)
-	/*other method : if (a % 2 == 0) find even number (with a++ instead a += 2)*/
-	
-		if (str[a] != '\0' && '\n')
+	for (a = 0; str[a] != '\0'; a ++)
 	{
+		if (a % 2 == 0)
+	
+		{
 			_putchar(str[a]);
+		}
 	}
 	_putchar('\n');
 }
