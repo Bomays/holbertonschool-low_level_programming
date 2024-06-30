a1431417fef4b3fdddd17eac95146aaad8d242e4
Bomays <9314@holbertonstudents.com>
2024-06-30 23:30:01 +0200

    100-atoi.c :  function that convert a string to an integer.

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,30 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
+#include "2-main.h"
 /**
- * main - Entry point
+ * _atoi - convert a string in a int
  *
- * Return: Always 0
+ * @s: points to a string
  *
+ * Return: product of num and sign
  */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
 
-int main(void)
+	while (*s)
+	{
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+		{
+			do {
+				num = num * 10 + (*s - '0');
+				s++;
+			} while (*s >= '0' && *s <= '9');
+			break;
+		}
+		s++;
+	}
+	return (num * sign);
+}
