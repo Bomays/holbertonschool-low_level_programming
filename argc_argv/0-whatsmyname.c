7ac531c94a4152af35e5db2d2199b1dbae93a08a
Bomays <9314@holbertonstudents.com>
2024-07-04 20:09:26 +0200

    0-whatsmyname.c: program that prints its name, followed by a new line.

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(int argc, char* argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
