4007d1d12d56ccc1d34f209c553c5521042de19d
Bomays <9314@holbertonstudents.com>
2024-07-05 13:40:26 +0200

    2-args.c: program that prints all arguments it receives.

---
+++ argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints all arguments it receives
+ * including the first one ; printing one arg per line ending
+ * with a new line
+ *
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char **argv)
+{
+	while(argc--)
+		printf("%s\n", *argv++);
+
+	return (0);
+}
