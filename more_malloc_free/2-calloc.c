38c67fdae2403dad391a8fb5c6216a833c6674e9
Bomays <9314@holbertonstudents.com>
2024-07-11 13:32:43 +0200

    2-calloc.c: correcting code using memset with string.h lib and malloc instead of calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * _calloc - function that allocates memory for an array, using malloc
  *
@@ -25,17 +26,21 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 
-	array = calloc(nmemb, sizeof(unsigned int));
+	/*array = calloc(nmemb, sizeof(unsigned int));*/
+	array = malloc(nmemb * sizeof(unsigned int));
 
 	if (array == NULL)
 	{
 		return (NULL);
 	}
+
+	/*using malloc + memset to set memory to 0 instead of only calloc*/
+	memset(array, 0, nmemb * size);
+
 	for (i = 0; i < size; i++)
 	{
 		array[i] = nmemb;
 	}
-	array[size] = '\0';
 
 	return (array);
 }
