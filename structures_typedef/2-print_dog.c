5ffe9fb3842b410fbdd6ee277b1d260c7dd5ea4c
Bomays <9314@holbertonstudents.com>
2024-07-12 15:15:46 +0200

    2-print_dog.c: output byte number correction with spaces

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,18 +15,18 @@ void print_dog(struct dog *d)
 			printf("(nil)\n");
 
 		else
-			printf("Name:%s\n", d->name);
+			printf("Name: %s\n", d->name);
 
 		if (d->age <= 0)
 			printf("(nil)\n");
 
 		else
-			printf("Age:%6f\n", d->age);
+			printf("Age: %6f\n", d->age);
 
 		if (d->owner == NULL)
 			printf("(nil)\n");
 
 		else
-			printf("Owner:%s\n", d->owner);
+			printf("Owner: %s\n", d->owner);
 	}
 }
