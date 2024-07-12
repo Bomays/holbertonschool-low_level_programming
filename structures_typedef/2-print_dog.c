bbfc1a9db009e422e0758bb1124d862f7ac1acce
Bomays <9314@holbertonstudents.com>
2024-07-12 15:31:10 +0200

    2-print_dog.c: adding forgotten words to be printed

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,19 +12,19 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if (d->name == NULL)
-			printf("(nil)\n");
+			printf("Name: (nil)\n");
 
 		else
 			printf("Name: %s\n", d->name);
 
 		if (d->age < 0)
-			printf("(nil)\n");
+			printf("Age: (nil)\n");
 
 		else
 			printf("Age: %6f\n", d->age);
 
 		if (d->owner == NULL)
-			printf("(nil)\n");
+			printf("Owner: (nil)\n");
 
 		else
 			printf("Owner: %s\n", d->owner);
