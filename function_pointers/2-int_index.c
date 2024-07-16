9f51f100c2353e1d43000009fd6315630ed3c17b
Bomays <9314@holbertonstudents.com>
2024-07-16 14:28:55 +0200

    2-int_index.c: - 124 timeout error of intranet checker - while loop deleted, it was just useless in the code making infinite loop i guess

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -24,13 +24,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	}
 
-	while (array && cmp && size > 0)
-		for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
 		{
-			if (cmp(array[i]))
-			{
-				return (i);
-			}
+			return (i);
 		}
+	}
 	return (-1);
 }
