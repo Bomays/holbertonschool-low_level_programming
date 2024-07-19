e1f389a123ffa379f7aceb54fe46bbc582fdac70
Bomays <9314@holbertonstudents.com>
2024-07-19 18:04:47 +0200

    3-print_all: variadic function output error - added a variable so types formats are preceded of a comma&space excepting the first one, instead adding it after them

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,52 +1,51 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include "variadic_functions.h"
-
 /**
  * print_all - variadic function that prints strings from different
  * types using switch for converting them fr printing,
  * followed by a new line
- *
  * @format: is a list of types of arguments passed to the function
  * Return: void
  */
 void print_all(const char * const format, ...)
 {
 	va_list arglist;
-	int i = 0;
-	char *chain;
-	char type;
+	int i = 0, arg_one = 1;
+	/*first arg initialized to 1 so comma will not be printed before it*/
+	char *chain, type;
 
 	va_start(arglist, format);
-
 	while (format && format[i])
 	{
-		type = format[i];
-		switch (type)
-		{
-			case 'c':
-			printf("%c", va_arg(arglist, int));
-			break;
-
-			case 'i':
-			printf("%d", va_arg(arglist, int));
-			break;
-
-			case 'f':
-			printf("%f", va_arg(arglist, double));
-			break;
-
-			case 's':
-			chain = va_arg(arglist, char*);
-			while (!chain)
-			printf("(nil)");
-			if (chain)
-			printf("%s", chain);
-			break;
-		}
-		if (format[i + 1] != '\0')
+		if (!arg_one)/*other arguments will have comma&space printed before*/
 		printf(", ");
-		i++;
+
+	type = format[i];
+	switch (type)
+	{
+		case 'c':
+			printf("%c", va_arg(arglist, int));
+			arg_one = 0;
+			break;
+		case 'i':
+			printf("%d", va_arg(arglist, int));
+			arg_one = 0;
+			break;
+		case 'f':
+			printf("%f", va_arg(arglist, double));
+			arg_one = 0;
+			break;
+		case 's':
+			chain = va_arg(arglist, char*);
+			if (chain)
+				printf("%s", chain);
+			if (!chain)
+				printf("(nil)");
+			arg_one = 0;
+			break;
+	}
+	i++;
 	}
 	va_end(arglist);
 	printf("\n");
