e0512de523814f4c28bc5f6fc6a50a6322ab77c4
Bomays <9314@holbertonstudents.com>
2024-07-31 11:04:33 +0200

    task 3: adding a node at the end of a single list duplicating str with strdup

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node_end - function that add a node at the end of a list
+ * @str: pointer to the string
+ * @head: pointer to pointer to head of singly list
+ *
+ * Return: return the new node
+ *
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *node, *temp;
+	unsigned int len = 0;
+
+	node = malloc(sizeof(*node));
+
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+	while (str[len] != 0)
+	{
+		len++;
+	}
+
+	node->str = strdup(str);
+	if (node->str == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+
+	node->len = len;
+	node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = node;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = node;
+	}
+	return (node);
+}
