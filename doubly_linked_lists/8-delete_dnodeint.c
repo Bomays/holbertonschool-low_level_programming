7c60a3bd52236b31830cfe200951876f193648ba
Bomays <9314@holbertonstudents.com>
2024-08-02 11:08:29 +0200

    Added task 8 file: fuction that delete a node at a given index in a double linked list

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - function that deletes
+ * a node at a given position
+ *
+ * @index: index of the list where the node has to be deleted
+ * @head: double pointer to head of node to delete
+ * Return: return 1 if it succeeded or -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *delete_node;
+	unsigned int i = 0;
+
+	if (*head == NULL || head == NULL)
+	{
+		return (-1);
+	}
+
+	delete_node = *head;
+		for (; i < index; i++)
+		{
+			delete_node = delete_node->next;
+		}
+	/**-------------------------------------*/
+	if (delete_node == NULL)
+	{
+		return (-1);
+	}
+	/**-------------------------------------*/
+	if (*head == delete_node)
+	{
+		*head = delete_node->next;
+	}
+	/**--------------------------------------*/
+	if (delete_node->next != NULL)
+	{
+		delete_node->next->prev = delete_node->prev;
+	}
+	/**---------------------------------------*/
+	if (delete_node->prev != NULL)
+	{
+		delete_node->prev->next = delete_node->next;
+	}
+	free(delete_node);
+	return (1);
+}
