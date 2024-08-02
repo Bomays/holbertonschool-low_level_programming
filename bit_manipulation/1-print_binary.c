ee387b0ae90c974b92f4c0f94b15e430deb009d0
Bomays <9314@holbertonstudents.com>
2024-08-02 16:55:47 +0200

    task1 file added:  function that prints a binary number from an unsigned int using maximum bit number

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_binary - function that prints a binary number from an unsigned int
+ *
+ * @n: the number that must be printed after been converted in binary
+ *
+ * Return: void return
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int printstart = 0;
+	unsigned int bitmax = 32768;/**highest bit for 16bit*/
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (bitmax)
+	{
+		if (printstart == 1 && (n & bitmax) == 0)
+		{
+			_putchar('0');
+		}
+		else if ((n & bitmax) != 0)
+		{
+			_putchar('1');
+			printstart = 1;
+		}
+	bitmax >>= 1;/**divide bitmax by 2 moving it to next lower bit*/
+		     /** until bitmax equals 0*/
+	}
+}
